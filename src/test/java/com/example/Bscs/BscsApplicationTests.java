3b800ab67781cd14f3e02c95ef91a2f66fe6c9c1
Ankz77 <ankundamwebembezi2@gmail.com>
2024-03-12 11:26:28 +0200

    data libraries for use

---
+++ src/test/java/com/example/Bscs/BscsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bscs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BscsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
